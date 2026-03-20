52f9d66e91dafb10235bbd560b50f620115f7974
v0t02tr <Vigneshwaran.T@walmart.com>
2026-03-21 02:48:22 +0530

    Initial commit

--- src/main/java/com/example/chatbot/controller/ChatController.java
+++ src/main/java/com/example/chatbot/controller/ChatController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class ChatController {
 
     private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
 
     @PostMapping
     public ChatResponse chat(@RequestBody ChatRequest request) {
